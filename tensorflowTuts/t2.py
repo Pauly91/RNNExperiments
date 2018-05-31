b1e77b5a5818d2336261d61a0537a4c5f12f6db9
Abel Babu <abelbabu@bgl-abelbabu-deb8-64>
2018-05-31 09:46:31 +0530

    Text generator RNN - Tutorial

--- tensorflowTuts/t2.py
+++ tensorflowTuts/t2.py
@@ -31,7 +31,7 @@ def imageTransformation(images):
     return images28
 
     
-def buildGraph():
+def NNBuildNRun(trainImages,trainLabels,testImages,testLabels):
     
     '''
     None implies any size
@@ -51,6 +51,9 @@ def buildGraph():
     # That is it should match the labels = [0,0..1, 0 ,00..] as opposed to [0.5,0.4..0,2.]
     loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, 
                                                                     logits = logits))
+    # sparse_softmax_cross_entropy_with_logits, label is an array of equal to the number of data
+    # pointa with each value is a class number.
+
     # Define an optimizer 
     train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
@@ -59,17 +62,47 @@ def buildGraph():
     # This returns the index off the largest value , alogn which dimension is to figured out 
     correct_pred = tf.argmax(logits, 1)
 
-    # Define an accuracy metric --- > Studied till here !!!!!!!!!!!!!!!!!!
+    # Define an accuracy metric --- > This does not make sense, take the index and then
+    # take the mean of it ?
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    tf.set_random_seed(1234)
+    print("images_flat: ", images_flat)
+    print("logits: ", logits)
+    print("loss: ", loss)
+    print("predicted_labels: ", correct_pred)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(201):
+            f, loss_value = sess.run([train_op, loss], feed_dict={x: trainImages, y: trainLabels})
+
+            print("Loss: ", loss)
+            print("f: ",f)
+        predicted = sess.run([correct_pred], feed_dict={x: testImages})[0]
+        # Calculate correct matches 
+        match_count = sum([int(y == y_) for y, y_ in zip(testLabels, predicted)])
+
+        # Calculate the accuracy
+        accuracy = match_count / len(testLabels)
+
+        # Print the accuracy
+        print("Accuracy: {:.3f}".format(accuracy))
+
+            # Read this for debugging: https://wookayin.github.io/tensorflow-talk-debugging/#1
 
 
 def main():
     ROOT_PATH = "/mathworks/home/abelbabu/pythonScripts/RNNExperiments/"
     train_data_directory = os.path.join(ROOT_PATH, "data/Training")
     test_data_directory = os.path.join(ROOT_PATH, "data/Testing")
-    images, labels = load_data(train_data_directory)
 
-    images = imageTransformation(images)
+    trainImages, trainLabels = load_data(train_data_directory)
+    testImages, testLabels = load_data(test_data_directory)
+
+
+    trainImages = imageTransformation(trainImages)
+    testImages = imageTransformation(testImages)
+    NNBuildNRun(trainImages,trainLabels,testImages,testLabels)
 
 if __name__ == '__main__':
     main()
